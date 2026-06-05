837d99bd479845daa34e267f54be44a5d484c04d
Manu <manulopezayora1985@protonmail.com>
2026-06-05 17:38:12 +0200

    chore (home-view): initial layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,12 @@ console.log(import.meta.env);
 </script>
 
 <template>
-    <main>
-        <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </main>
+    <div class="flex flex-col lg:flex-row">
+        <header class="flex-1">
+            <h1 class="text-3xl font-bold underline">Hello world!</h1>
+        </header>
+        <main class="flex-1">
+            <h1 class="text-3xl font-bold underline">Hello world!</h1>
+        </main>
+    </div>
 </template>
