2f76fd268920b20caaf40e78a48ecdf3f6e75933
Manu <manulopezayora1985@protonmail.com>
2026-06-08 16:06:41 +0200

    chore (header): fix position sticky

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,30 +1,51 @@
 <template>
-    <header class="px-5 py-10 flex flex-col gap-5 lg:flex-1 lg:min-h-dvh lg:sticky lg:top-1 lg:justify-between">
+    <header
+        class="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24"
+    >
         <div class="flex flex-col gap-5">
-            <h1 class="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">Manuel Jesús López Martínez</h1>
-            <h2 class="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">Senior frontend developer</h2>
-            <p class="mt-4 max-w-xs leading-normal">Especializado en la construcción de experiencias digitales robustas, escalables y centradas en el usuario con Angular, React y Vue</p>
+            <h1 class="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
+                Manuel Jesús López Martínez
+            </h1>
+            <h2 class="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
+                Senior frontend developer
+            </h2>
+            <p class="mt-4 max-w-xs leading-normal">
+                Especializado en la construcción de experiencias digitales robustas, escalables y
+                centradas en el usuario con Angular, React y Vue
+            </p>
 
             <nav class="flex-col hidden lg:flex">
                 <RouterLink to="/" class="group flex gap-1 items-center hover:text-slate-200">
-                    <span id="nav-line" class="bg-slate-400 w-12 h-0.5 block transition-all ease-in-out duration-300 group-hover:w-20 group-hover:bg-slate-200"></span>
+                    <span
+                        id="nav-line"
+                        class="bg-slate-400 w-12 h-0.5 block transition-all ease-in-out duration-300 group-hover:w-20 group-hover:bg-slate-200"
+                    ></span>
                     <span class=".nav-link">About</span>
                 </RouterLink>
                 <RouterLink to="/" class="group flex gap-1 items-center hover:text-slate-200">
-                    <span id="nav-line" class="bg-slate-400 w-12 h-0.5 block transition-all ease-in-out duration-300 group-hover:w-20 group-hover:bg-slate-200"></span>
+                    <span
+                        id="nav-line"
+                        class="bg-slate-400 w-12 h-0.5 block transition-all ease-in-out duration-300 group-hover:w-20 group-hover:bg-slate-200"
+                    ></span>
                     <span class=".nav-link">Experience</span>
                 </RouterLink>
                 <RouterLink to="/" class="group flex gap-1 items-center hover:text-slate-200">
-                    <span id="nav-line" class="bg-slate-400 w-12 h-0.5 block transition-all ease-in-out duration-300 group-hover:w-20 group-hover:bg-slate-200"></span>
+                    <span
+                        id="nav-line"
+                        class="bg-slate-400 w-12 h-0.5 block transition-all ease-in-out duration-300 group-hover:w-20 group-hover:bg-slate-200"
+                    ></span>
                     <span class=".nav-link">Projects</span>
                 </RouterLink>
                 <RouterLink to="/" class="group flex gap-1 items-center hover:text-slate-200">
-                    <span id="nav-line" class="bg-slate-400 w-12 h-0.5 block transition-all ease-in-out duration-300 group-hover:w-20 group-hover:bg-slate-200"></span>
+                    <span
+                        id="nav-line"
+                        class="bg-slate-400 w-12 h-0.5 block transition-all ease-in-out duration-300 group-hover:w-20 group-hover:bg-slate-200"
+                    ></span>
                     <span class=".nav-link">Articles</span>
                 </RouterLink>
             </nav>
         </div>
-        <ul class="social-links flex gap-5">
+        <ul class="flex gap-5 ml-1 mt-8">
             <li>
                 <RouterLink to="/" class="hover:text-slate-200">
                     <GitHubIcon class="h-8" />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,9 +5,11 @@ import MainComponent from '@/components/MainComponent.vue';
 
 <template>
     <div class="relative">
-        <div class="mx-auto min-h-screen max-w-7xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0 lg:flex">
-            <HeaderComponent />
-            <MainComponent />
+        <div class="mx-auto min-h-screen max-w-7xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
+            <div class="lg:flex lg:justify-between lg:gap-4">
+                <HeaderComponent />
+                <MainComponent />
+            </div>
         </div>
     </div>
 </template>
