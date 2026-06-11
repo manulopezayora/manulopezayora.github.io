2efdc9128ab081d2444fd1c5f08dee49b0b92940
Manu <manulopezayora1985@protonmail.com>
2026-06-11 16:31:14 +0200

    chore (articles): Add article temporal component

---
+++ src/components/ArticlesComponent.vue
@@ -0,0 +1,235 @@
+<template>
+    <section
+        id="writing"
+        class="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+        aria-label="Blog posts"
+    >
+        <div
+            class="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0"
+        >
+            <h2 class="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
+                Writing
+            </h2>
+        </div>
+        <div>
+            <ul class="group/list">
+                <li class="mb-12">
+                    <div
+                        class="group relative grid grid-cols-8 gap-4 transition-all sm:items-center sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50"
+                    >
+                        <div
+                            class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"
+                        ></div>
+                        <img
+                            alt='Welcome to Black Parade by My Chemical Romance text with "Black" changed to "AI"'
+                            loading="lazy"
+                            width="200"
+                            height="48"
+                            decoding="async"
+                            data-nimg="1"
+                            class="z-10 col-span-2 aspect-video rounded border-2 border-slate-200/10 object-cover transition group-hover:border-slate-200/30 sm:col-span-2"
+                            style="color: transparent"
+                            srcset="
+                                /_next/image?url=%2Fimages%2Fwriting%2Fai-parade.png&amp;w=256&amp;q=75 1x,
+                                /_next/image?url=%2Fimages%2Fwriting%2Fai-parade.png&amp;w=640&amp;q=75 2x
+                            "
+                            src="/_next/image?url=%2Fimages%2Fwriting%2Fai-parade.png&amp;w=640&amp;q=75"
+                        />
+                        <div class="z-10 col-span-6">
+                            <p class="-mt-1 text-sm font-semibold leading-6">2026</p>
+                            <h3 class="-mt-1">
+                                <a
+                                    class="inline-flex items-baseline font-medium leading-tight text-slate-200 group/link text-base"
+                                    href="https://medium.com/@bchiang7/welcome-to-the-ai-parade-765f79b00c30"
+                                    target="_blank"
+                                    rel="noreferrer noopener"
+                                    aria-label="Welcome to the AI Parade (opens in a new tab)"
+                                    ><span
+                                        class="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"
+                                    ></span
+                                    ><span
+                                        >Welcome to the AI<!-- -->
+                                        <span class="inline-block"
+                                            >Parade<svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
+                                                class="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                                                aria-hidden="true"
+                                            >
+                                                <path
+                                                    fill-rule="evenodd"
+                                                    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                                    clip-rule="evenodd"
+                                                ></path></svg></span></span
+                                ></a>
+                            </h3>
+                        </div>
+                    </div>
+                </li>
+                <li class="mb-12">
+                    <div
+                        class="group relative grid grid-cols-8 gap-4 transition-all sm:items-center sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50"
+                    >
+                        <div
+                            class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"
+                        ></div>
+                        <img
+                            alt="Accessibility icon"
+                            loading="lazy"
+                            width="200"
+                            height="48"
+                            decoding="async"
+                            data-nimg="1"
+                            class="z-10 col-span-2 aspect-video rounded border-2 border-slate-200/10 object-cover transition group-hover:border-slate-200/30 sm:col-span-2"
+                            style="color: transparent"
+                            srcset="
+                                /_next/image?url=%2Fimages%2Fwriting%2Fa11y.png&amp;w=256&amp;q=75 1x,
+                                /_next/image?url=%2Fimages%2Fwriting%2Fa11y.png&amp;w=640&amp;q=75 2x
+                            "
+                            src="/_next/image?url=%2Fimages%2Fwriting%2Fa11y.png&amp;w=640&amp;q=75"
+                        />
+                        <div class="z-10 col-span-6">
+                            <p class="-mt-1 text-sm font-semibold leading-6">2024</p>
+                            <h3 class="-mt-1">
+                                <a
+                                    class="inline-flex items-baseline font-medium leading-tight text-slate-200 group/link text-base"
+                                    href="https://klaviyo.tech/5-common-accessibility-pitfalls-and-how-to-avoid-them-e316dfc55ff0"
+                                    target="_blank"
+                                    rel="noreferrer noopener"
+                                    aria-label="5 Common Accessibility Pitfalls and How to Avoid Them (opens in a new tab)"
+                                    ><span
+                                        class="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"
+                                    ></span
+                                    ><span
+                                        >5 Common Accessibility Pitfalls and How to Avoid<!-- -->
+                                        <span class="inline-block"
+                                            >Them<svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
+                                                class="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                                                aria-hidden="true"
+                                            >
+                                                <path
+                                                    fill-rule="evenodd"
+                                                    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                                    clip-rule="evenodd"
+                                                ></path></svg></span></span
+                                ></a>
+                            </h3>
+                        </div>
+                    </div>
+                </li>
+                <li class="mb-12">
+                    <div
+                        class="group relative grid grid-cols-8 gap-4 transition-all sm:items-center sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50"
+                    >
+                        <div
+                            class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"
+                        ></div>
+                        <img
+                            alt="Telescope"
+                            loading="lazy"
+                            width="200"
+                            height="48"
+                            decoding="async"
+                            data-nimg="1"
+                            class="z-10 col-span-2 aspect-video rounded border-2 border-slate-200/10 object-cover transition group-hover:border-slate-200/30 sm:col-span-2"
+                            style="color: transparent"
+                            srcset="
+                                /_next/image?url=%2Fimages%2Fwriting%2Falgolia-search.jpeg&amp;w=256&amp;q=75 1x,
+                                /_next/image?url=%2Fimages%2Fwriting%2Falgolia-search.jpeg&amp;w=640&amp;q=75 2x
+                            "
+                            src="/_next/image?url=%2Fimages%2Fwriting%2Falgolia-search.jpeg&amp;w=640&amp;q=75"
+                        />
+                        <div class="z-10 col-span-6">
+                            <p class="-mt-1 text-sm font-semibold leading-6">2020</p>
+                            <h3 class="-mt-1">
+                                <a
+                                    class="inline-flex items-baseline font-medium leading-tight text-slate-200 group/link text-base"
+                                    href="https://medium.com/stories-from-upstatement/integrating-algolia-search-with-wordpress-multisite-e2dea3ed449c"
+                                    target="_blank"
+                                    rel="noreferrer noopener"
+                                    aria-label="Integrating Algolia Search with WordPress Multisite (opens in a new tab)"
+                                    ><span
+                                        class="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"
+                                    ></span
+                                    ><span
+                                        >Integrating Algolia Search with WordPress<!-- -->
+                                        <span class="inline-block"
+                                            >Multisite<svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
+                                                class="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                                                aria-hidden="true"
+                                            >
+                                                <path
+                                                    fill-rule="evenodd"
+                                                    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                                    clip-rule="evenodd"
+                                                ></path></svg></span></span
+                                ></a>
+                            </h3>
+                        </div>
+                    </div>
+                </li>
+                <li class="mb-12">
+                    <div
+                        class="group relative grid grid-cols-8 gap-4 transition-all sm:items-center sm:gap-8 md:gap-4 lg:hover:!opacity-100 lg:group-hover/list:opacity-50"
+                    >
+                        <div
+                            class="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"
+                        ></div>
+                        <img
+                            alt="Headless horseman"
+                            loading="lazy"
+                            width="200"
+                            height="48"
+                            decoding="async"
+                            data-nimg="1"
+                            class="z-10 col-span-2 aspect-video rounded border-2 border-slate-200/10 object-cover transition group-hover:border-slate-200/30 sm:col-span-2"
+                            style="color: transparent"
+                            srcset="
+                                /_next/image?url=%2Fimages%2Fwriting%2Fheadless.jpeg&amp;w=256&amp;q=75 1x,
+                                /_next/image?url=%2Fimages%2Fwriting%2Fheadless.jpeg&amp;w=640&amp;q=75 2x
+                            "
+                            src="/_next/image?url=%2Fimages%2Fwriting%2Fheadless.jpeg&amp;w=640&amp;q=75"
+                        />
+                        <div class="z-10 col-span-6">
+                            <p class="-mt-1 text-sm font-semibold leading-6">2019</p>
+                            <h3 class="-mt-1">
+                                <a
+                                    class="inline-flex items-baseline font-medium leading-tight text-slate-200 group/link text-base"
+                                    href="https://medium.com/stories-from-upstatement/building-a-headless-mobile-app-cms-from-scratch-bab2d17744d9"
+                                    target="_blank"
+                                    rel="noreferrer noopener"
+                                    aria-label="Building a Headless Mobile App CMS From Scratch (opens in a new tab)"
+                                    ><span
+                                        class="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"
+                                    ></span
+                                    ><span
+                                        >Building a Headless Mobile App CMS From<!-- -->
+                                        <span class="inline-block"
+                                            >Scratch<svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20"
+                                                fill="currentColor"
+                                                class="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                                                aria-hidden="true"
+                                            >
+                                                <path
+                                                    fill-rule="evenodd"
+                                                    d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                                                    clip-rule="evenodd"
+                                                ></path></svg></span></span
+                                ></a>
+                            </h3>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+</template>
